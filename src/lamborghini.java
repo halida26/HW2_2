6960d4ac70ec43a740158c81646b5ca605083c01
halida26 <sydygalievahalida2@gmail.com>
2023-11-13 15:00:47 +0300

    second commit

---
+++ src/lamborghini.java
@@ -0,0 +1,27 @@
+public class lamborghini extends Car {
+    @Override
+    public void print() {
+        System.out.println(getInfo() + "interior Color" + getInteriorColor() + " coubtry :" + getCountry());
+    }
+
+    private String interiorColor;
+    private String country;
+
+    public lamborghini(String model, String color, int year, String interiorColor, String country) {
+        super(model, color, year);
+        this.interiorColor = interiorColor;
+        this.country = country;
+    }
+
+    public String getInteriorColor() {
+        return interiorColor;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public lamborghini(String model, String color, int year) {
+        super(model, color, year);
+    }
+}
